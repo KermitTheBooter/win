c1571a10fb107ae09c33a0a9c0ca909bedffbad5
KermitTheBooter <61944383+KermitTheBooter@users.noreply.github.com>
2023-08-13 18:05:28 -0400

    Update win.lua

--- win.lua
+++ win.lua
@@ -1,15 +1,15 @@
-local verioncheck = loadstring(game:HttpGet("https://pastebin.com/raw/a8nbi425"))()
+local verioncheck = loadstring(game:HttpGet("https://pastebin.com/raw/FcUmebyb"))()
 local version = "Version10"
 for i,v in pairs(verioncheck) do 
     print(v)
     if v == version then
 print("Verion Check Pass")
-        local blacklistcheck = loadstring(game:HttpGet("\104\116\116\112\115\058\047\047\112\097\115\116\101\098\105\110\046\099\111\109\047\114\097\119\047\056\048\076\056\087\077\072\078"))()
+        local blacklistcheck = loadstring(game:HttpGet("https://pastebin.com/raw/Yq4pdWu3"))()
         if blacklistcheck[game:service('Players').LocalPlayer.UserId] then
         game:service('Players').LocalPlayer:Kick('Blacklisted | Appeal at https://discord.gg/zzsr3ReE4s')
         else
             print("blacklist check pass 1")
-            local NameCheck = loadstring(game:HttpGet("https://pastebin.com/raw/S9uBetxH"))()
+            local NameCheck = loadstring(game:HttpGet("https://pastebin.com/raw/1TXdbjZF"))()
                 if NameCheck[game:service("Players").LocalPlayer.Name] then 
                     game:service('Players').LocalPlayer:Kick('Blacklisted | Appeal at https://discord.gg/zzsr3ReE4s')
                 else
